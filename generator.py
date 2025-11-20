c21e0b2aa28d2b63fe07060ec2ca862dd3c73b21
Philip Mueller <philip.mueller@cscs.ch>
2025-11-20 10:47:35 +0100

    Small refactoring.

--- generator.py
+++ generator.py
@@ -47,31 +47,28 @@ def write_project_index(
     )
 
     normalized_project_name = normalize_name(project_name)
-    anchor_elements: list[str] = []
-    for file in project_folder.iterdir():
-        filename = file.name
-        if filename.startswith("."):
-            continue
-        elif not any(filename.endswith(ext) for ext in [".zip", ".tar.gz", ".whl"]):
-            continue
-        assert filename.startswith(normalized_project_name + "-")
-
-        # Compute the hash such that we can append it to the link.
-        with open(file, "rb") as F:
-            digest = hashlib.file_digest(F, "sha256")
-
-        # PEP503 says that the text of the anchor element must be the filename, so there
-        #  is not need for fancy processing of the file name. Furthermore, we assume that
-        #  the file names have the correct normalized name and version.
-        anchor_elements.append(
-                f'\t\t<a href="{filename}#sha256={digest.hexdigest()}">{filename}</a> </br>\n'.replace("\t", "    ")
-        )
-
-    # Now write the index file.
     with open(project_folder / "index.html", "wt") as index:
-        title = f"Custom Package for '{project_name}'"
-        index.write(HTML_HEADER.format(Title=title))
-        index.writelines(anchor_elements)
+        index.write(HTML_HEADER.format(Title=f"Custom Package for '{project_name}'"))
+
+        for file in project_folder.iterdir():
+            filename = file.name
+            if filename.startswith("."):
+                continue
+            elif not any(filename.endswith(ext) for ext in [".zip", ".tar.gz", ".whl"]):
+                continue
+            assert filename.startswith(normalized_project_name + "-")
+
+            # Compute the hash such that we can append it to the link.
+            with open(file, "rb") as F:
+                digest = hashlib.file_digest(F, "sha256")
+
+            # PEP503 says that the text of the anchor element must be the filename, so there
+            #  is not need for fancy processing of the file name. Furthermore, we assume that
+            #  the file names have the correct normalized name and version.
+            index.write(
+                    f'\t\t<a href="{filename}#sha256={digest.hexdigest()}">{filename}</a> </br>\n'.replace("\t", "    ")
+            )
+
         index.write(HTML_FOOTER)
 
 
@@ -94,7 +91,6 @@ def write_package_index(
             write_project_index(base_folder, project_name)
 
         index.write(HTML_FOOTER)
-
 
 
 if __name__ == "__main__":
