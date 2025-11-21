b037e2532caa3c0a17fde31535661599c4a213f6
Philip Mueller <philip.mueller@cscs.ch>
2025-11-21 08:44:31 +0100

    Found out that the name has to match.

--- issue_dispatch.sh
+++ issue_dispatch.sh
@@ -19,4 +19,4 @@ curl -L \
   -H "Authorization: Bearer $(cat .token)" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/${OWNER}/${REPO}/dispatches \
-  -d '{"event_type":"on-demand-test","client_payload":{"source_repo":"'"${SOURCE_REPO}"'","source_org":"'"${SOURCE_ORG}"'","dependency_ref":"'"${DEPENDENCY_REF}"'"}}'
+  -d '{"event_type":"update_package_index","client_payload":{"source_repo":"'"${SOURCE_REPO}"'","source_org":"'"${SOURCE_ORG}"'","dependency_ref":"'"${DEPENDENCY_REF}"'"}}'
