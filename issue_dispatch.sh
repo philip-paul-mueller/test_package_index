3da74179673a42c646c868a853461cb8e1180f20
Philip Mueller <philip.mueller@cscs.ch>
2025-11-21 08:48:21 +0100

    Added some documentation.

--- issue_dispatch.sh
+++ issue_dispatch.sh
@@ -9,11 +9,13 @@ DEPENDENCY_REF="gt4py-next-integration"
 
 if [ ! -e ".token" ]
 then
-	echo "Token does not exist."
+	echo "The file with the token, '.token' does not exist."
+	echo " According to 'https://docs.github.com/en/rest/repos/repos?apiVersion=2022-11-28#create-a-repository-dispatch-event'"
+	echo " a fine grained token with '\"Contents\" repository permissions (write)' is needed."
 	exit 1
 fi
 
-curl -L \
+curl -L -v \
   -X POST \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $(cat .token)" \
